7d319ae78b3cefddeda2847407106043ca544956
EC2 Default User <ec2-user@ip-172-31-39-165.eu-west-2.compute.internal>
2022-04-21 11:28:05 +0000

    Activity 15 completed

--- 13/activity13.sh
+++ 13/activity13.sh
@@ -6,31 +6,58 @@ touch notunique1.txt
 rm notunique2.txt
 touch notunique2.txt
 
-
 cat test1.txt | while read num; do
-
-	unique="True"
 
 	cat test2.txt | while read num2; do
 
 		echo "$num and $num2"
 
-		awk
-
 		if [[ "$num" == "$num2" ]]; then
 			echo "$num is not unique"
 			echo "$num" >> notunique1.txt
-			break
 		fi
 
 	done
 
 done
 
-cat test1.txt | while read num; do
-	match=`echo grep $num notunique1.txt`
-	if [[ "$match" == "" ]]; then
-		echo "$num is unique"
-	fi
+file1_array=(`cat "test1.txt"`)
+file2_array=(`cat "test2.txt"`)
+
+non_unique_numbers=(`cat "notunique1.txt"`)
+
+echo "Only in file1: "
+
+file1_length=`echo ${#file1_array[@]}`
+file2_length=`echo ${#file2_array[@]}`
+non_unique_numbers_length=`echo ${#non_unique_numbers[@]}`
+
+for ((i = 0 ; i < $non_unique_numbers_length ; i++)); do
+
+	for ((j = 0 ; j < $file1_length ; j++)); do
+
+		if [[ file1_array[$j] -eq non_unique_numbers[$i] ]]; then
+			unset file1_array[$j]
+		fi
+	done
+
+	for ((j = 0 ; j < $file2_length ; j++)); do
+		if [[ file2_array[$j] -eq non_unique_numbers[$i] ]]; then
+			unset file2_array[$j]
+		fi
+	done
+
+echo "Only in file 1:"
+
+for i in "${file1_array[@]}"; do
+	echo $i
 done
 
+echo "Only in file 2:"
+
+for i in "${file2_array[@]}"; do
+	echo $i
+done
+
+	
+done
--- 15/activity15.sh
+++ 15/activity15.sh
@@ -2,8 +2,6 @@
 
 cd ~
 
-ls
-
 if [[ ! -d "FixGen/logs" ]]; then
 	echo "hi"
 
@@ -17,17 +15,17 @@ fi
 
 cd FixGen/logs
 
-./fixGenerator.sh &
+#./fixGenerator.sh &
 
 number_of_lines=`ls -ltr | grep 'fixlog.*' | wc -l`
 
 echo "$number_of_lines lines"
 
-lines=`ls -ltr` | grep 'fixlog.*'
-
 number_to_compress=$((number_of_lines - 3))
 
-for ((i = 0 ; i < $number_to_compress ; i++)); do
-	
-done
+files_to_compress=`ls -ltr ~/FixGen/logs/ | grep fixlog | head -n $number_to_compress | awk '{print $9}'`
 
+echo "The files that will be compressed are below:"
+echo $files_to_compress
+
+tar -cvf log_tarball.tar $files_to_compress
